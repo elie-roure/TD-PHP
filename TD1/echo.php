38cc481317ce39695feb667bbfa67001ea467aaf
Elie Roure <elie.roure@etu.umontpellier.fr>
2020-09-11 16:13:48 +0200

    TD1 Exo4 Affichage de variables

--- TD1/echo.php
+++ TD1/echo.php
@@ -8,7 +8,7 @@
     <body>
         Voici le résultat du script PHP : 
         <?php
-          // Ceci est un commentaire PHP sur une ligne
+          /*// Ceci est un commentaire PHP sur une ligne
           /* Ceci est le 2ème type de commentaire PHP
           sur plusieurs lignes */
            
@@ -17,7 +17,42 @@
           $texte = "hello world !";
 
           // On écrit le contenu de la variable 'texte' dans la page Web
-          echo $texte;
+          /*echo $texte;
+
+          $prenom = "Marc";
+
+echo "Bonjour " . $prenom;
+echo "Bonjour $prenom";
+echo 'Bonjour $prenom';
+
+echo $prenom;
+echo "$prenom";*/
+          //$voitures = array();
+          
+          $voitures = array(
+            'marque' => 'Seat',
+            'couleur' => 'Gris',
+            'immatriculation' => 'GH 584 HB' );
+
+          echo "<h1>Liste des voitures : </h1>";
+          if (empty($voitures)) {
+            echo "Il n'y a aucune voiture.";
+          }
+          echo "<ul>";
+          foreach ($voitures as $key => $value) {
+            echo "<li>$key : $value</li>";
+            # code...
+          }
+          
+          echo "</ul>";
+         // var_dump($voitures);
+          /*$marque = "Seat";
+          $couleur = "Gris";
+          $immatriculation = "GH 584 HB";
+
+          echo "<p>Voiture $immatriculation de marque $marque (couleur $couleur)</p>";*/
+
+
         ?>
     </body>
 </html> 
