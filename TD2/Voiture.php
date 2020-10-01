f13d57eff2aad551657c4f3c353c16cc8b66ed95
Roure Elie <elie.roure@etu.umontpellier.fr>
2020-10-01 16:14:50 +0200

    fin TD2

--- TD2/Voiture.php
+++ TD2/Voiture.php
@@ -1,6 +1,6 @@
 <?php
 class Voiture {
- 
+
   private $marque;
   private $couleur;
   private $immatriculation;
@@ -35,15 +35,27 @@ public function setImmatriculation($immatriculation2) {
 }
 
   // un constructeur
-public function __construct($m, $c, $i)  {
- $this->marque = $m;
- $this->couleur = $c;
- $this->immatriculation = $i;
-} 
-
+public function __construct($m = NULL, $c = NULL, $i = NULL) {
+  if (!is_null($m) && !is_null($c) && !is_null($i)) {
+    // Si aucun de $m, $c et $i sont nuls,
+    // c'est forcement qu'on les a fournis
+    // donc on retombe sur le constructeur à 3 arguments
+    $this->marque = $m;
+    $this->couleur = $c;
+    $this->immatriculation = $i;
+  }
+}
   // une methode d'affichage.
 public function afficher() {
   echo "<p>Voiture $this->immatriculation de marque $this->marque (couleur $this->couleur)</p> ";
+}
+
+
+public function getAllVoitures(){
+  $rep = (Model::$pdo)->query("Select * From voiture");
+  $rep->setFetchMode(PDO::FETCH_CLASS, 'Voiture');
+  $tab_voit = $rep->fetchAll();
+  return $tab_voit;
 }
 }
 ?>
--- TD2/lireVoiture.php
+++ TD2/lireVoiture.php
@@ -4,19 +4,24 @@ require_once('Voiture.php');
 
 Model::Init();
 
-$rep = (Model::$pdo)->query("Select * From voiture");
+/*$rep = (Model::$pdo)->query("Select * From voiture");
 
 $tab_obj = $rep->fetchAll(PDO::FETCH_OBJ);
 foreach($tab_obj as $value){
 	print_r($value);
 	echo "</br>";
-}
+}*/
 
-/*$rep->setFetchMode(PDO::FETCH_CLASS, 'Voiture');
+/*$rep->setFetchMode(PDO::FETCH_CLASS, 'voiture');
 $tab_voit = $rep->fetchAll();
 foreach ($tab_voit as $value) {
 	echo $value->afficher();
 	echo "</br>";
 }*/
+
+foreach (Voiture::getAllVoitures() as $value) {
+	echo ($value->afficher());
+	echo "<br>";
+	}
 
 ?>
