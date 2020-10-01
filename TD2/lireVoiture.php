c76cf764afcb1850309328a661e571c285c6a70e
Roure Elie <elie.roure@etu.umontpellier.fr>
2020-10-01 15:53:10 +0200

    table voiture v minuscule

--- TD2/lireVoiture.php
+++ TD2/lireVoiture.php
@@ -4,12 +4,19 @@ require_once('Voiture.php');
 
 Model::Init();
 
-$rep = (Model::$pdo)->query("Select * From Voiture");
+$rep = (Model::$pdo)->query("Select * From voiture");
 
 $tab_obj = $rep->fetchAll(PDO::FETCH_OBJ);
 foreach($tab_obj as $value){
-    print_r($value);
-    echo "</br>";
+	print_r($value);
+	echo "</br>";
 }
 
- ?>
+/*$rep->setFetchMode(PDO::FETCH_CLASS, 'Voiture');
+$tab_voit = $rep->fetchAll();
+foreach ($tab_voit as $value) {
+	echo $value->afficher();
+	echo "</br>";
+}*/
+
+?>
