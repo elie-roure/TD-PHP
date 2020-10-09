067bcdfa5f7d33ba9cd113d2e9d124d747bd1a65
Elie Roure <elie.roure@etu.umontpellier.fr>
2020-10-09 15:35:03 +0200

    Exo5

---
+++ TD4/controller/ControllerVoiture.php
@@ -0,0 +1,8 @@
+<?php
+require_once ('../model/ModelVoiture.php'); // chargement du modèle
+class ControllerVoiture {
+    public static function readAll() {
+        $tab_v = ModelVoiture::getAllVoitures();     //appel au modèle pour gerer la BD
+        require ('../view/voiture/list.php');  //"redirige" vers la vue
+    }
+}
